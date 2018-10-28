af210ac40d18dfdf9006986acf3afb0d088ea356
PavelDevelop <paveldsirius@gmail.com>
2018-10-28 17:55:37 +0300

    init

---
+++ Data/ShopContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Shop.Data
+{
+    public class ShopContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<GiftCard> GiftCards { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<Product>()
+                .HasMany(x => x.OrderItems);
+
+            modelBuilder.Entity<Order>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<Order>()
+                .HasMany<OrderItem>();
+
+            modelBuilder.Entity<OrderItem>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(x => x.Order)
+                .WithMany(x => x.Items)
+                .HasForeignKey(x => x.OrderId);
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(x => x.Product);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ShopContextDb;AttachDbFilename=C:\\ShopDb.mdf;");
+        }
+    }
+}
